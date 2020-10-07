b5a593033115306ca74007c58f90f8f1f2be5f37
wei <wei@weide-MacBook-Pro.local>
2020-10-07 14:19:03 +0800

    Answer Checking Function Updated

--- Scheduler/static/js/exam.js
+++ Scheduler/static/js/exam.js
@@ -64,6 +64,23 @@
         },
         success: function(response){
           console.log("Success");
+          
+          $('input[name="option"]:checked').each(function(){
+            if (response.answer.includes($(this).attr('id'))){
+              console.log("Right!");
+              $('label[for="'+$(this).attr('id')+'"]').css('color','green',);
+              response.answer.splice(response.answer.indexOf($(this).attr('id')), 1);
+            }
+            else{
+              console.log("Wrong!");
+              $('label[for="'+$(this).attr('id')+'"]').css('color','red',);
+            }
+          });
+          response.answer.forEach(element => {
+            $('label[for="'+element+'"]').css('color','red',);
+          });
+          
+          swiper.slideTo(0);
         },
         error: function(response){
           console.log("Failed");
