0cb0f39f19263650dd4a11e2500b75ec27bda1e5
wei <wei@weide-MacBook-Pro.local>
2020-10-12 08:53:12 +0800

    Add Event Bug Fixxed

--- Scheduler/WeekSchedule/views.py
+++ Scheduler/WeekSchedule/views.py
@@ -23,30 +23,22 @@ def EventPage(request):
 			description = form.cleaned_data['description']
 			start_time = form.cleaned_data['start_time']
 			clock = form.cleaned_data['clock']
-			minute = clock * 30
-			end_time = start_time + datetime.timedelta(minutes = minute)
-			NewEvent = Event.objects.get_or_create(
-				user = request.user,
-				subject = subject,
-				description = description,
-				start_time = start_time,
-				clock = clock,
-				end_time = end_time,
-			)
 
-			event = {
-				'subject': NewEvent[0].subject,
-				'description': NewEvent[0].description,
-				'start_time': NewEvent[0].start_time,
-				'clock': NewEvent[0].clock,
-				'end_time': NewEvent[0].end_time,
-			}
-
-			data = {
-				'event': event,
-			}
 			
-			return JsonResponse(data, status = 200)	
+			for i in range(clock):
+				end_time = start_time + datetime.timedelta(minutes = 30)
+				Event.objects.get_or_create(
+					user = request.user,
+					subject = subject,
+					description = description,
+					start_time = start_time,
+					clock = clock,
+					end_time = end_time,
+				)
+				start_time = start_time + datetime.timedelta(minutes = 30)
+
+			
+			return JsonResponse({}, status = 200)	
 		else:
 
 			data = {
